7a7757ad3195324ce8cc5dd6b4e3c6a08bedbd9e
MithSi <mithunganesh@gmail.com>
2014-07-01 21:14:57 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <ul class="footer-default-links muted">
     <li>&copy; Kheyos <?php echo date('Y'); ?></li>
     <li>&middot;</li>
-    <li><a href="about.php">About</a></li>
+    <li><a href="http://about.kheyos.com/">About</a></li>
     <li>&middot;</li>
     <li><a href="http://blog.kheyos.com" target="_blank">Blog</a></li>
     <li>&middot;</li>
